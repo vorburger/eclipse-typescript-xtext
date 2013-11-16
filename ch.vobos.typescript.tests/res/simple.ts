c174b9e4c0793c356ba0dae40896d04630e06b45
lbovet <laurent.bovet@windmaster.ch>
2013-11-16 18:06:39 +0100

    Doc comments work!

--- ch.vobos.typescript.tests/res/simple.ts
+++ ch.vobos.typescript.tests/res/simple.ts
@@ -1,6 +1,15 @@
-
+/* no doc comment  */
+/** 
+ * doc comment 
+ */
 interface VobosVeryFirstTypeScriptStruct {
+    /** 
+     * the name 
+     */
 	name: string;
+	/**
+	 * the number
+	 */
 	nummerae: number
 	parent: VobosVeryFirstTypeScriptStruct 
-}
+}  
